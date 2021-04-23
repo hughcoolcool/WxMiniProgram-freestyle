0c470bf8d86d98db2a049d381100474da02801a1
hugh <hdgangganggang@gmail.com>
2021-04-23 22:13:36 +0800

    Initial Commit

---
+++ pages/mainPage/mainPage.js
@@ -0,0 +1,40 @@
+// pages/mainPage/mainPage.js
+
+//导入js
+var util = require('../../utils/util.js')
+Page({
+  data: {
+    slider: [],
+    swiperCurrent: 0
+  },
+  onLoad: function () {
+    var that = this;
+//网络访问，获取轮播图的图片
+    util.getRecommend(function(data){
+      that.setData({
+        slider: data.data.slider
+      })
+    }); 
+  },
+  //轮播图的切换事件
+  swiperChange: function(e){
+//只要把切换后当前的index传给<swiper>组件的current属性即可
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
+  },
+  //点击指示点切换
+  chuangEvent: function(e){
+    this.setData({
+      swiperCurrent: e.currentTarget.id
+    })
+  },
+
+  battle(){
+    
+    wx.navigateTo({
+      url: '/pages/battleOption/battleOption',
+    })
+  }
+})
+ 
